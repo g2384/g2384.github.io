9c10d8bbeabdbdae81c0c88a8fbf6c87a2da919e
g2384 <hileony@gmail.com>
2014-12-30 17:36:25 +0000

    Create WordFrequencyCount.py

---
+++ work/WordFrequencyCount.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+#support Chinese
+#format the file by replacing ([，。：“”‘’（）《》！、；？]) with \r\n
+dictionary={}
+c=0
+threshold=1#minimum number of duplicates
+max_length=7#maximum length of word
+min_length=2
+string = open('input.txt','r').read()
+string=unicode(string,"utf-8")
+print('finish reading file')
+arr=string.split()
+
+for n in range(min_length,max_length):
+    for i in arr:
+        for k in range (0,n):
+            array=[i[u:u+n] for u in range(k, len(i), n)]
+            for j in array:
+                if(len(j)>=n):
+                    if not j in dictionary:  
+                        dictionary[j]=0
+                    dictionary[j]+=1
+print('lenth of dict: '+ str(len(dictionary)))
+
+for i in dictionary.keys():
+    if (dictionary[i]<threshold):
+        dictionary.pop(i, None)
+print('lenth of filtered dict: '+ str(len(dictionary)))
+
+#if "abc" with 2 counts, and "abcdef" with 2 counts
+#they actually refer to the same word, i.e. "abc" is in "abcdef"
+for i in dictionary.keys():
+    for j in dictionary.keys():
+        if not i in dictionary:
+            break
+        if (j in i) and len(i)!=len(j) and dictionary[j]==dictionary[i]:
+            print ('"'+j+'"('+str(dictionary[j])+') is merged into "'+i+'"('+str(dictionary[i])+')')
+            c+=1
+            dictionary.pop(j, None)
+            break
+print('number of merged entries: '+str(c))
+
+f=open('result.txt','w')            
+for i in dictionary.keys():
+    if(dictionary[i]>=threshold):
+        f.write (i.encode('utf-8')+'\t'+str(dictionary[i])+'\n')
+        print(i+'\t'+str(dictionary[i]))
+f.close()  
